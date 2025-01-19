b3c6db3f397c8eef7375f7a0a11502bbb2497443
Liam <liam3323@gmail.com>
2025-01-19 23:01:33 +0000

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,8 @@
-import type { NextConfig } from "next";
-
-const isProd: boolean = process.env.NODE_ENV === 'production';
-
-interface NextConfig {
-  reactStrictMode: boolean;
-  images: {
-    unoptimized: boolean;
-  };
-  assetPrefix: string;
-  basePath: string;
-  output: string;
-}
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/your-repository-name/' : '',
-  basePath: isProd ? '/your-repository-name' : '',
   output: 'export',
+  basePath: '/liamo3323.github.io',
 };
 
 export default nextConfig;
