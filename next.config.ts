c3161e2abcf96c1e527cfe448f2e30bf547a276e
Liam <liam3323@gmail.com>
2025-01-19 21:01:46 +0000

    deploy basic proj

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
+const isProd: boolean = process.env.NODE_ENV === 'production';
+
+interface NextConfig {
+  reactStrictMode: boolean;
+  images: {
+    unoptimized: boolean;
+  };
+  assetPrefix: string;
+  basePath: string;
+  output: string;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/your-repository-name/' : '',
+  basePath: isProd ? '/your-repository-name' : '',
+  output: 'export',
 };
 
 export default nextConfig;
