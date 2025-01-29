b02ba6ef3ae262888b6c467279505103766ab219
Liam <liam3323@gmail.com>
2025-01-29 22:37:04 +0000

    setup github actions

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  }
   basePath: '/liamo3323.github.io',
+  assetPrefix: '/liamo3323.github.io',
 };
 
 export default nextConfig;
